3420e414247d462bd179a5d45999d1be77fc57c3
adslucvantien <ads.lucvantien@gmail.com>
2023-08-29 17:36:23 +0700

    V1

---
+++ AdvanceMenu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMenu
+{
+    public interface IUserInterface
+    {
+        void WriteLine(string message);
+        string ReadLine();
+        int ReadInt();
+    }
+
+
+    public class AdvanceMenu
+    {
+        private string[] menuItems;
+        private int languageChoice;
+        private IUserInterface userInterface;
+
+        public AdvanceMenu(string[] items, IUserInterface ui, int initialLanguageChoice = 1)
+        {
+            menuItems = items;
+            userInterface = ui;
+            languageChoice = initialLanguageChoice;
+        }
+
+        public int Display()
+        {
+            userInterface.WriteLine("Menu:");
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                userInterface.WriteLine($"{i + 1}. {menuItems[i]}");
+            }
+
+            int choice = GetValidChoice();
+            return choice;
+        }
+
+        private int GetValidChoice()
+        {
+            while (true)
+            {
+                userInterface.WriteLine("Enter your choice: ");
+                int choice = userInterface.ReadInt();
+
+                if (choice >= 1 && choice <= menuItems.Length)
+                {
+                    return choice;
+                }
+                else
+                {
+                    userInterface.WriteLine("Invalid choice. Please enter a valid number.");
+                }
+            }
+        }
+
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,50 @@
+﻿namespace LibMenu
+{
+    public class Menu
+    {
+            private string[] menuItems;
+            private int languageChoice;
+
+            public Menu(string[] items, int initialLanguageChoice = 1)
+            {
+                menuItems = items;
+                languageChoice = initialLanguageChoice;
+            }
+
+            public int Display()
+            {
+                Console.WriteLine("Menu:");
+                for (int i = 0; i < menuItems.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {menuItems[i]}");
+                }
+
+                int choice = GetValidChoice();
+                return choice;
+            }
+
+            private int GetValidChoice()
+            {
+                while (true)
+                {
+                    Console.Write("Enter your choice: ");
+                    string input = Console.ReadLine() ?? string.Empty;
+                
+                    if (int.TryParse(input,out int choice) && choice >= 1 && choice <= menuItems.Length)
+                    {
+                        return choice;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid choice. Please enter a valid number.");
+                    }
+                }
+            }
+
+
+
+
+    }
+
+    
+}
